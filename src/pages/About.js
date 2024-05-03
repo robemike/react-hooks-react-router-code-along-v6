189ee29726c44525abc2ff83eaf8459df238bded
robemike <robemike1135@gmail.com>
2024-05-03 20:11:40 +0300

    completes tests

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+       <header>
+        {/* Save space for NavBar */}
+      </header>
+      <main>
+        <h1>This is my about component!</h1>
+      </main>
+    </div>
+  )
+}
+
+export default About
